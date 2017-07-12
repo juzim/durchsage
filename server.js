7abd835e7270cacdb24a6ab7475849d89e222a1e
Julian <julianzimmermann@gmail.com>
2017-07-12 18:09:48 +0200

    Removed ugly quickfix

--- server.js
+++ server.js
@@ -36,41 +36,49 @@ const sendResponse = (request, response, data) => {
 }
 
 app.get('/v1', function(req, res) {
-  const file = req.query.file
-  if (file === undefined || !fs.existsSync('config/' + file + '.json')) {
-    sendResponse(req, res, {"success": false, "text": "No config file '" + file + "' found"})
+  try {
+
+    const file = req.query.file
+    if (file === undefined || !fs.existsSync('config/' + file + '.json')) {
+      throw "Config file '" + file + "' not found"
+    } else {
+      process.env.NODE_ENV = file
+    }
+
+    var config = require('config');
+
+    const list = config.get("values")
+    console.log('New request')
+
+    const action = getRandomFromList('action', config.get("actions"))
+
+    var text = config.get("texts")[action]
+
+    if (config.has("texts.prefix")) {
+      text = config.get("texts.prefix") + text
+    }
+
+    var i = 0;
+    while (text.match(/{(\w+)}/)) {
+      i++
+      if (i > 10) {
+        throw "Too many loops while parsing placeholders for " + text
+      }
+      text = text.replace(/{(\w+)}/g, function (w, m) {
+        if (list[m] == undefined) {
+          throw "Could not match placeholder " + m
+        }
+        return getRandomFromList(m, list[m]);
+      });
+    }
+  } catch (e) {
+    sendResponse(req, res, {"success": false, "text": e})
     return
-  } else {
-    process.env.NODE_ENV = file
   }
 
-  var config = require('config');
-
-  const list = config.get("values")
-  console.log('New request')
-
-  const action = getRandomFromList('action', config.get("actions"))
-
-  let text = config.get("texts")[action]
-
-  if (config.has("texts.prefix")) {
-    text = config.get("texts.prefix") + text
-  }
-
- //	const result = text.replace('{train}', train).replace('{gleis}', gleis).replace('{city}', city).replace('{action}', action);
- 	 text = text.replace(/{(\w+)}/g, function (w, m) {
- 		return getRandomFromList(m, list[m]);
- });
- // dirty way to handle nested placeholders
- text = text.replace(/{(\w+)}/g, function (w, m) {
-   return getRandomFromList(m, list[m]);
-});
-
- console.log("Result: " + text);
-
-   sendResponse(req, res, {"text": text})
+  console.log("Result: " + text);
+  sendResponse(req, res, {"text": text})
 })
-
 
 var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
